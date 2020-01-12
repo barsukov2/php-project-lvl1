c622c2f3bcb17767ffad5271492c05a028335925
barsukov2 <barsukov2@gmail.com>
2020-01-12 12:01:34 +0400

    cosmetic cli.php upd

--- src/Cli.php
+++ src/Cli.php
@@ -5,9 +5,8 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function run()
-{
-    line('Welcome to the Brain Game!');
+function run() {
+    line('Welcome to the Brain Game!'); 
     $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
+    line('Hello, %s!', $name);
 }
